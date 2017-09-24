12afeffede36f6f199b6ece05f1b060c148518aa
cliff <cliff_ok@outlook.com>
2017-09-24 12:34:03 -0400

    Initial commit

--- app/src/main/java/com/example/cliff/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/cliff/androidlabs/LoginActivity.java
@@ -2,12 +2,46 @@ package com.example.cliff.androidlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends Activity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(TAG,"In onCreate()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"In onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"In onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"In onDestroy()");
     }
 }
